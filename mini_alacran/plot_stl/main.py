95b2a44b6013d795e17b02127d974ce39780011a
IkedaTogo <syuugo151122@gmail.com>
2022-01-20 15:57:42 +0900

    up

--- mini_alacran/plot_stl/main.py
+++ mini_alacran/plot_stl/main.py
@@ -18,6 +18,10 @@ axes = mplot3d.Axes3D(figure)
 body_mesh = mesh.Mesh.from_file('../stl/low_model/body.stl')
 left_arm_mesh = mesh.Mesh.from_file('../stl/low_model/flipper-arm.stl')
 right_arm_mesh = mesh.Mesh.from_file('../stl/low_model/flipper-arm.stl')
+copy_body_mesh = body_mesh
+
+print(copy_body_mesh)
+print(body_mesh)
 
 # 位置の調整
 body_init_pos = [0,0,-53.036]
@@ -28,11 +32,12 @@ body_mesh = MeshAdj.mesh_location_zero(body_mesh,body_init_pos)
 left_arm_mesh = MeshAdj.mesh_location_zero(left_arm_mesh,left_arm_init_pos)
 right_arm_mesh = MeshAdj.mesh_location_zero(right_arm_mesh,right_arm_init_pos)
 
+
 R = np.zeros((4, 4))
 R[3,3] = 1
 R[:3, :3] = tuple(RotationMatrix.x(90)) #回転行列
 R[:3,3:] = [[0],[0],[0]]                #並進ベクトル
-print("rotation matrix is:",R)
+# print("rotation matrix is:",R)
 body_mesh.transform(R)
 left_arm_mesh.transform(R)
 right_arm_mesh.transform(R)
@@ -40,14 +45,14 @@ right_arm_mesh.transform(R)
 
 # 最終出力
 cube_comb = mesh.Mesh(np.concatenate([
-    body_mesh.data.copy(),
+    copy_body_mesh.data.copy(),
     left_arm_mesh.data.copy(),
     right_arm_mesh.data.copy(),
 ]))
 # axes.add_collection3d(mplot3d.art3d.Poly3DCollection(cube_comb.vectors))
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(cube_comb.vectors,color='w',alpha=0.4,edgecolor='k'))
 
-print(cube_comb.max_[0] - cube_comb.min_[0])
+# print(cube_comb.max_[0] - cube_comb.min_[0])
 
 scale = cube_comb.points.flatten()
 axes.auto_scale_xyz(scale, scale, scale)
--- mini_alacran/plot_stl/test/animation_test2.py
+++ mini_alacran/plot_stl/test/animation_test2.py
@@ -5,7 +5,7 @@ from functools import partial
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import animation
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection,Line3DCollection
 from mpl_toolkits.mplot3d import Axes3D
 from stl import mesh
 
@@ -25,7 +25,8 @@ class SimulationWorld():
         ax = fig.add_subplot(111, projection='3d', proj_type='persp')
         self.scale = self.robots.final_robot.points.flatten()
         ax.auto_scale_xyz(self.scale, self.scale, self.scale) 
-        collection = Poly3DCollection(self.robots.final_robot.vectors,color='w',alpha=0.4,edgecolor='k') 
+        collection = Poly3DCollection(self.robots.final_robot.vectors,color='w',alpha=0.2,edgecolor='k') 
+        # collection = Line3DCollection(self.robots.final_robot.vectors, colors='k', linewidths=0.2, linestyles=':')
         ax.add_collection3d(collection)
         anim = animation.FuncAnimation(fig, self.one_step, fargs=(collection,ax),
                                     frames=360, interval=1000 / 30,blit=True)
@@ -42,7 +43,7 @@ class SimulationWorld():
             #     obj.draw(ax, collection)
             if hasattr(obj, "one_step"):
                 obj.one_step(frame,ax,collection,self.scale)
-        ax.auto_scale_xyz([-100,100], [-100,100], [-100,100]) 
+        ax.auto_scale_xyz([-150,150], [-150,150], [-150,150]) 
         return collection,ax
 
 class Robot():
@@ -57,51 +58,63 @@ class Robot():
             init_param = parts_data[2]
             mesh_data =MeshAdj.mesh_location_zero(mesh_data,init_param)
             self.parts_add(parts_name,mesh_data)
-            self.parts_rotation(parts_name, 90)
+            self.init_rotation(parts_name, 90)
 
 
-        self.first_robot_state = self.robot_mesh
-        
-    
     def parts_add(self,parts_name,mesh_data):
         self.parts_name_list.append(parts_name)
-        self.robot_mesh[parts_name] = mesh_data
+        self.robot_mesh[parts_name] = [mesh_data, 0]
         self.robot_update()
-    
-    def parts_rotation(self, parts_name, deg):
+
+    def init_rotation(self, parts_name, deg):
         R = np.zeros((4, 4))
         R[3,3] = 1
         R[:3, :3] = tuple(RotationMatrix.x(deg)) #回転行列
         R[:3,3:] = [[0],[0],[0]]
-        self.robot_mesh[parts_name].transform(R)
+        self.robot_mesh[parts_name][0].transform(R)
         self.robot_update()
+
+    def parts_rotation(self, parts_name, deg):
+        now_deg = self.robot_mesh[parts_name][1]
+        move_deg = deg - now_deg
+        R = np.zeros((4, 4))
+        R[3,3] = 1
+        R[:3, :3] = tuple(RotationMatrix.x(move_deg)) #回転行列
+        R[:3,3:] = [[0],[0],[0]]
+        self.robot_mesh[parts_name][0].transform(R)
+        self.robot_mesh[parts_name][1] = deg
+        self.robot_update()
+
     
     def robot_update(self):
         comb_robot=[]
+        # meshデータの更新
         for i in range(len(self.parts_name_list)):
-            self.robot_mesh[self.parts_name_list[i]] = MeshAdj.mesh_update(self.robot_mesh[self.parts_name_list[i]])
+            self.robot_mesh[self.parts_name_list[i]][0] = MeshAdj.mesh_update(self.robot_mesh[self.parts_name_list[i]][0])
+        # meshデータの結合
         for mesh_data in self.robot_mesh.values(): 
-            comb_robot.append(mesh_data.data.copy())
+            comb_robot.append(mesh_data[0].data.copy())
         self.final_robot = mesh.Mesh(np.concatenate(comb_robot))
         return self.final_robot 
     
     def parts_center_point(self):
         points = []
         for mesh_data in self.robot_mesh.values():
-            points.append(MeshAdj.get_mesh_center(mesh_data))
+            points.append(MeshAdj.get_mesh_center(mesh_data[0]))
         return points
 
 
     def one_step(self,frame, ax,collection,scale):
-        self.parts_rotation("body",1)
-        # self.parts_rotation("left arm",-1)
-        # self.parts_rotation("right arm",2)
+        self.parts_rotation("body",-20)
+        # print("frame",-30)
+        self.parts_rotation("left arm",20)
+        self.parts_rotation("right arm",-20)
         points = np.array(self.parts_center_point())
         print("point=",points)
         ax.clear()
         collection.set_verts(self.final_robot.vectors)
         collection.do_3d_projection(collection.axes.get_figure().canvas.get_renderer())
-        ax.scatter(points[:,0:1], points[:,1:2], points[:,2:3], s = 400, c = "blue")
+        ax.scatter(points[:,0:1], points[:,1:2], points[:,2:3], s = 200, c = "red")
         # for ln in points: c = ax.scatter(points[0], points[1], points[2], s=100, marker="*", label="landmarks", color="orange")
 
 
