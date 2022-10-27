8dad263575ac12cb7bad65a4ff8524137e5785e0
IkedaTogo <syuugo151122@gmail.com>
2022-10-27 20:30:22 +0900

    Update figure_test.py

--- groundpoint_estimate/test/2D_test/figure_test.py
+++ groundpoint_estimate/test/2D_test/figure_test.py
@@ -1,25 +1,54 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import numpy as np
 
-fig = plt.figure(figsize=[5,5])
-ax = plt.axes()
+def setting_graph(ax):
+    plt.minorticks_on()
+    ax.grid(which="major",alpha=0.6)
+    ax.grid(which="minor",alpha=0.3)
+    plt.hlines([0], xmin, xmax, "blue", linestyles='dashed')     # hlines
+    plt.axis('scaled')
+    ax.set_aspect('equal')
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
 
-# fc = face color, ec = edge color
-# c = patches.Circle(xy=(0, 0), radius=0.5, fc='g', ec='r')
-# e = patches.Ellipse(xy=(-0.25, 0), width=0.5, height=0.25, fc='b', ec='y')
-body = patches.Rectangle(xy=(-69, -128), width=138, height=150, ec='#000000', fill=False)
-left_arm = patches.Rectangle(xy=(-90, -20), width=13.2, height=126, ec='#000000', fill=False)
-right_arm = patches.Rectangle(xy=(76.8, -20), width=13.2, height=126, ec='#000000', fill=False)
-# r = patches.Rectangle(xy=(0, 0), width=0.25, height=0.5, ec='#000000', fill=False)
+def alacran_model(ax):
+    body = patches.Rectangle(xy=(-69, -128), width=138, height=150, ec='#000000', fill=False)
+    left_arm = patches.Rectangle(xy=(-90, -20), width=13.2, height=126, ec='#000000', fill=False)
+    right_arm = patches.Rectangle(xy=(76.8, -20), width=13.2, height=126, ec='#000000', fill=False)
+    ax.add_patch(body)
+    ax.add_patch(left_arm)
+    ax.add_patch(right_arm)
 
-plt.minorticks_on()
-ax.grid(which="major",alpha=0.6)
-ax.grid(which="minor",alpha=0.3)
-ax.add_patch(body)
-ax.add_patch(left_arm)
-ax.add_patch(right_arm)
+def line():
 
-plt.axis('scaled')
-ax.set_aspect('equal')
+    left_arm_range = np.array([[-83.4 ,0] ,[-83.4 ,88.5]]) 
+    
+    right_arm_range =np.array([[83.4,0] ,[83.4,88.5]]) 
 
-plt.show()
+    body_range =np.array([[0,-36.],
+                        [-69. ,-65.78417266],
+                        [-69. ,-109.25 ],
+                        [69 ,-109.25 ],
+                        [69 ,-65.78417266]]) 
+    
+    plt.plot(left_arm_range[:,:1], left_arm_range[:,1:2],color="green")
+    plt.plot(right_arm_range[:,:1], right_arm_range[:,1:3],color="green")
+    patch = patches.Polygon(xy=body_range[:,:2], closed=True)
+    ax.add_patch(patch)
+    print("left_arm_range",left_arm_range[:,:1], left_arm_range[:,1:3])
+    print("right_arm_range",right_arm_range[:,:1], right_arm_range[:,1:3])
+    print("body ",body_range[:,:2])
+
+
+if __name__ == "__main__":
+    fig = plt.figure(figsize=[8,8])
+    ax = plt.axes()
+    xmin,xmax = -150,150
+    ymin,ymax = -150,150
+    
+    setting_graph(ax)
+    alacran_model(ax)
+    line()
+    
+    plt.show()
