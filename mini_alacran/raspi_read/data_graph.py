7aa8f5f064abc094e6393ec2bf6271b973eda119
IkedaTogo <syuugo151122@gmail.com>
2023-01-08 22:40:02 +0900

    Update data_graph.py

--- mini_alacran/raspi_read/data_graph.py
+++ mini_alacran/raspi_read/data_graph.py
@@ -2,11 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # df = pd.read_csv('robot_data.csv', names=['num1', 'num2','num3'])
-df = pd.read_csv('robot_data.csv', names=['num1', 'num2','num3','num4','num5'])
+df = pd.read_csv('data/heimen_setti.csv', names=['num1', 'num2','num3','num4','num5','num6'])
 plt.figure()
 plt.plot(df['num1'],df['num2'],label='roll')
 plt.plot(df['num1'],df['num3'],label= "pich")
 plt.plot(df['num1'],df['num4'],label= "left_deg")
 plt.plot(df['num1'],df['num5'],label= "right_deg")
+plt.legend()
 # plt.plot(range(0,count),df['num1'],marker="o",markersize=2)
 plt.show()
